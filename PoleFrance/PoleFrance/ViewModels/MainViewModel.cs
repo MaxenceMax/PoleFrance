59f31f5572ebff6487275e793a78d9bce9a9b86e
MaxenceMax <maxence.montet01@gmail.com>
2016-04-04 12:21:18 +0200

    Changes CSS and form

--- PoleFrance/PoleFrance/ViewModels/MainViewModel.cs
+++ PoleFrance/PoleFrance/ViewModels/MainViewModel.cs
@@ -10,11 +10,17 @@ namespace PoleFrance.Models
         public Candidature Candidature { get; set; }
         public Pole Pole { get; set; }
         public bool Redirection { get; set; }
+        public bool Internat { get; set; }
+        public bool AutorisationParent { get; set; }
 
         public InformationSportive Information1 { get; set; }
         public InformationSportive Information2 { get; set; }
         public InformationSportive Information3 { get; set; }
 
         public InformationScolaire InformationScolaire { get; set; }
+        public SouhaitScolaire SouhaitScolaire { get; set; }
+        public Langue LV1 { get; set; }
+        public Langue LV2 { get; set; }
+        public Options option { get; set; }
     }
 }
