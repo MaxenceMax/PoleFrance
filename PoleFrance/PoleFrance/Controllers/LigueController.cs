0bc5908599069b03c5dfaac58226968cf0b4925b
MaxenceMax <maxence.montet01@gmail.com>
2016-04-27 16:21:54 +0200

    Ajout Controleur

---
+++ PoleFrance/PoleFrance/Controllers/LigueController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PoleFrance.Controllers
+{
+    public class LigueController : Controller
+    {
+        public ActionResult LigueHome()
+        {
+            return View();
+        }
+    }
+}
